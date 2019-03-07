d1a909d8bdeaff27e9d3dbd1091de605e2e31cb3
MengInBM <zhengdongxing@bmsoft.com.cn>
2019-03-07 10:39:14 +0800

    创建

---
+++ src/main/java/com/tencent/gaio/GovernmentApplication.java
@@ -0,0 +1,17 @@
+package com.tencent.gaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableFeignClients//启动Feign客户端
+@EnableRedisHttpSession//启动使用redis实现session共享
+public class GovernmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovernmentApplication.class, args);
+    }
+
+}
